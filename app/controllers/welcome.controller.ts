3d7b1166f81268e8d779ae66979b215eb75ff58d
pawelEr <rosz.pawel@gmail.com>
2018-07-20 21:12:19 +0200

    Added file extension detection.

--- app/controllers/welcome.controller.ts
+++ app/controllers/welcome.controller.ts
@@ -1,6 +1,9 @@
 // Import only what we need from express
 import { Router, Request, Response } from 'express';
 import { readdirSync, copyFileSync, mkdirSync, existsSync } from 'fs';
+import { sync } from 'read-chunk';
+import FileType from 'file-type';
+import { FileTypeResult } from 'file-type'
 
 // Assign router to the express.Router() instance
 const router: Router = Router();
@@ -9,8 +12,6 @@ const router: Router = Router();
 export class WelcomeController {
     private router: Router
     private imagesPath: string = process.env.LocalAppData + "\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets"
-
-
 
     constructor() {
         this.router = Router();
@@ -25,7 +26,7 @@ export class WelcomeController {
         this.router.get('/:name', this.getName)
     }
 
-    private prepareUploadsDir(): void{
+    private prepareUploadsDir(): void {
         if (!existsSync('upload'))
             mkdirSync('upload')
     }
@@ -45,8 +46,14 @@ export class WelcomeController {
         let files: string[] = readdirSync(this.imagesPath);
 
 
-        let fileEntrys: FileEntry[] = files.map((file) => { return new FileEntry(file, this.imagesPath, '/upload'); })
-        fileEntrys.forEach((fileEntry) => { copyFileSync(fileEntry.inputPath, "." + fileEntry.uploadedPath) });
+        let fileEntrys: FileEntry[] = files.map((file) => {
+
+            return new FileEntry(file, this.imagesPath, '/upload');
+        })
+        fileEntrys.forEach((fileEntry) => {
+
+            copyFileSync(fileEntry.inputPath, "." + fileEntry.uploadedPath)
+        });
 
 
         let fileList: string[] = fileEntrys.map((file) => { return file.uploadedPath });
@@ -70,6 +77,8 @@ class FileEntry {
     constructor(fileName: string, inputPath: string, uploadedPath: string) {
         this.fileName = fileName;
         this.inputPath = inputPath + "\\" + fileName;
-        this.uploadedPath = uploadedPath + "\\" + fileName;
+        let buffer: Buffer = sync(this.inputPath, 0, 4100);
+        let type: FileTypeResult = FileType(buffer);
+        this.uploadedPath = uploadedPath + "\\" + fileName + "." + type.ext;
     }
 }
